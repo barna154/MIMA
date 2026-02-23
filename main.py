f6a993c87849d65ae33c6270e2a7603845027273
barna154 <barnax.craft@gmail.com>
2026-02-23 11:25:10 +0100

    a

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ atlagoskor = pd.read_csv(
     r"adatbazisok\stadat-sza0069-24.2.1.21-hu.csv",
     sep=";",          # mert valószínűleg pontosvesszős
     encoding="latin1"  # Windows-1250 / latin1 a magyar CSV-knél
+    header=2
 )
 
 otodik_sor = atlagoskor.iloc[4]  
 
 print(otodik_sor)
 
-atlag_kor_oszlop = atlagoskor["Év"]
-print(atlag_kor_oszlop)
+print(atlagoskor.head())
+print("Oszlopok:", atlagoskor.columns.tolist())
