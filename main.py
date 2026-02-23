25a9501b2af37dca2dc9135392fdfb00a7bc030c
barna154 <barnax.craft@gmail.com>
2026-02-23 12:02:24 +0100

    a

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ ertekek_sorted = ertekek.sort_values(ascending=False)
 # Grafikon
 plt.figure(figsize=(10,6))
 plt.bar(ertekek_sorted.index, ertekek_sorted.values)
-plt.title(f"Járművek száma - {sor['Év']} ({sor['Időszak']})")
+plt.title(f"Járművek száma - {sor['Év']}")
 plt.ylabel("Darabszám")
 plt.show()
