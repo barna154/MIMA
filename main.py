14308b2bf91bab57f41ce3f04389189e2cccd58c
barna154 <barnax.craft@gmail.com>
2026-02-23 11:17:35 +0100

    v

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import pandas as pd
-import os
-print("Jelenlegi munkakönyvtár:", os.getcwd())
+import pandas as pd 
 
 atlagoskor = pd.read_csv(
     r"adatbazisok\stadat-sza0069-24.2.1.21-hu.csv",
-    sep=";",          # mert valószínűleg pontosvesszős
     encoding="latin1"  # Windows-1250 / latin1 a magyar CSV-knél
 )
 
