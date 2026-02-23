f99aa9e9c08dec7053891ef109e06375e7cf2636
barna154 <barnax.craft@gmail.com>
2026-02-23 11:33:13 +0100

    a

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pandas as pd
 atlagoskor = pd.read_csv(
     r"adatbazisok\stadat-sza0069-24.2.1.21-hu.csv",
     sep=";",          # mert valószínűleg pontosvesszős
-    encoding="latin1",  # Windows-1250 / latin1 a magyar CSV-kné
+    encoding="latin1",  # Windows-1250 / latin1 a magyar CSV-knél
+
 )
 
-otodik_sor = atlagoskor.iloc[2]  
+otodik_sor = atlagoskor.iloc[0]  
 
 print(otodik_sor)
