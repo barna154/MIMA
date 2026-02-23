d50a8a2ff262436fb9b33e2c243d41c56e39098d
barna154 <barnax.craft@gmail.com>
2026-02-23 12:20:45 +0100

    a

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ atlagoskor = pd.read_csv(
 atlagoskor.columns = atlagoskor.columns.str.strip()
 
 # Üres 'Év' kitöltése
-atlagoskor['Év'] = atlagoskor['Év'].ffill()
+atlagoskor['Ev'] = atlagoskor['Ev'].ffill()
 
 # Csak azok a sorok, ahol a Személygépkocsi oszlop **számot tartalmaz**  
 # (előző próbálkozás helyett, regex használat)
@@ -28,14 +28,14 @@ jun_sorok = atlagoskor[
     atlagoskor['Személygépkocsi'].str.replace(' ', '').str.match(r'^\d+$', na=False) &
     atlagoskor['Idoszak'].str.contains('június', na=False)
 ]# X és Y
-x = jun_sorok['Év'].str.replace('.', '', regex=False)
+x = jun_sorok['Ev'].str.replace('.', '', regex=False)
 y = jun_sorok['Személygépkocsi']
 
 # Grafikon
 plt.figure(figsize=(12,6))
 plt.plot(x, y, marker='o')
 plt.title("Személygépkocsik száma június végén")
-plt.xlabel("Év")
+plt.xlabel("Ev")
 plt.ylabel("Darabszám")
 plt.gca().yaxis.set_major_formatter(mtick.StrMethodFormatter('{x:,.0f}'))
 plt.grid(True)
