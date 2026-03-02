0bfc96b8373ba1fb7abd8ec5107d2bd64d2cc39a
barna154 <barnax.craft@gmail.com>
2026-03-02 12:27:59 +0100

    frissítés

--- lakossag.py
+++ lakossag.py
@@ -4,8 +4,9 @@ import matplotlib.ticker as mtick
 df = pd.read_csv(
     r"adatbazisok\stadat-nep0034-22.1.2.1-hu.csv",
     sep=";",
-    encoding="cp1250",
+    encoding="latin2",   # <<< EZ A LÉNYEG
     header=1
 )
 
+df.columns = df.columns.str.strip()
 print(df.columns.tolist())
