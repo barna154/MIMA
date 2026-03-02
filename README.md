7c5d6628cb254258c795a23d455338a6009e0b5f
barna154 <barnax.craft@gmail.com>
2026-03-02 10:36:05 +0100

    frissites

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Mesterséges Intelligencia Mérnöki Alkalmazásai beadandó
 
 <h1 align="center">A hazai autóállomány szerkezeti elemzése és nemzetközi összefüggései</h1>
 
+
+
 ![Projekt logó](kepek/osszes_hu.png)
--- teljes_allomany.py
+++ teljes_allomany.py
@@ -1,31 +1,63 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
+# CSV beolvasása
 df = pd.read_csv(
-    r"adatbazisok\24.1.2.2. A közúti gépjárművek száma vármegye és r.txt",
+    r"adatbazisok\stadat-sza0040-24.1.2.2-hu.csv",
     sep=";",
-    encoding="cp1250"  # ha így jó a magyar ékezet
+    encoding="cp1250",
+    header=1
 )
 
-# Csak a 'Magyarország összesen' sor, személygépkocsi blokk
-mask_total = (df["Területi egység neve"] == "Magyarország összesen") & \
-             (df["Területi egység szintje"] == "ország")
+# Magyarország összesen sor kiválasztása (személygépkocsi blokk)
+mask = (df["Területi egység neve"] == "Magyarország összesen") & \
+       (df["Területi egység szintje"] == "ország")
 
-total_row = df[mask_total].iloc[0]
+orszagos = df[mask].iloc[0]
 
-years = [str(y) for y in range(2000, 2025)]
-values = (
-    total_row[years]
+# Évek kigyűjtése (2000–2024)
+evszamok = [str(ev) for ev in range(2000, 2025)]
+
+# Értékek átalakítása
+ertekek = (
+    orszagos[evszamok]
     .astype(str)
-    .str.replace(" ", "", regex=False)  # ezredelés szóköz eltávolítása
+    .str.replace(" ", "", regex=False)
     .astype(int)
 )
 
-plt.figure(figsize=(12, 6))
-plt.bar(years, values)
-plt.xticks(rotation=45)
-plt.title("Személygépkocsik száma Magyarországon (dec. 31., 2000–2024)")
-plt.ylabel("Darabszám")
+# Diagram
+plt.figure(figsize=(14, 7), facecolor="#DEDCDC")
+x_pos = range(len(evszamok))
+
+plt.bar(x_pos, ertekek, color="#8BF43F")
+
+# Feliratok az oszlopok tetején
+for i, v in enumerate(ertekek):
+    plt.text(
+        i,
+        v + max(ertekek)*0.005,
+        f"{v:,}".replace(",", " "),
+        ha="center",
+        va="bottom",
+        fontsize=10,
+        color="black",
+        fontweight="bold"
+    )
+
+plt.title("Magyarország teljes személygépkocsi-állománya (2000–2024)",
+          color="black", size=23, fontweight="bold")
+
+plt.xlabel("Év", color="black", size=20, fontweight="bold")
+plt.ylabel("Darabszám", color="black", size=20, fontweight="bold")
+
+plt.xticks(x_pos, evszamok, rotation=45)
+plt.gca().yaxis.set_major_formatter(mtick.StrMethodFormatter("{x:,.0f}"))
+plt.gca().tick_params(axis="x", colors="black")
+plt.gca().tick_params(axis="y", colors="black")
+plt.gca().set_facecolor("#DEDCDC")
+plt.grid(axis="y", linestyle="--", alpha=0.6, color="green")
 
 plt.tight_layout()
 plt.show()
