ad068d040fadb6dea465d7b10b5fd3bfda8fba46
barna154 <barnax.craft@gmail.com>
2026-03-09 11:21:33 +0100

    frissites

---
+++ projekt.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from scipy.stats import randint
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split, RandomizedSearchCV 
+from sklearn import metrics 
+from sklearn.tree import plot_tree
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = pd.read_csv('2024.csv')
+
+data.head()
